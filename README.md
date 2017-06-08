fabb6c774593d0c59f1be9513157328d98f4977c
Rochelle Lewis <rlewis37@gmail.com>
2017-06-08 08:42:03 -0600

    reset project

--- README.md
+++
@@ -1,2 +0,0 @@
-# ng-debug
-Project for debugging Angular2 and Webpack issues.
--- public_html/templates/home.php
+++
@@ -1,7 +0,0 @@
-<section class="welcome">
-	<div class="container">
-		<div class="jumbotron text-center">
-			<h1>Welcome to the Angular 2 Debug Project!</h1>
-		</div>
-	</div>
-</section>
